3db6acc404ef0ec16753d245fa4ffc3d9d9235fe
Habsucht <aleksey.aka.gremlin@gmail.com>
2017-07-15 01:19:33 +0300

    Fixed a test CheckSortCountries in class CheckSort

--- java-example/src/test/java/ru/stqa/selenium_training/tests/ui/CheckSort.java
+++ java-example/src/test/java/ru/stqa/selenium_training/tests/ui/CheckSort.java
@@ -22,20 +22,21 @@ public class CheckSort extends BaseTests {
 
         List<WebElement> countries = wd.findElements(By.xpath("//table[@class='dataTable']//tr[@class='row']"));
 
-        System.out.println(countries.size());
+        System.out.println("Найдено " + countries.size() + " стран");
 
-        for (int i = 1; i < countries.size(); i++) {
-            unsortedCountriesList.add(countries.get(i).findElement(By.xpath("//td[5]")).getText());
-            sortedCountriesList.add(countries.get(i).findElement(By.xpath("//td[5]")).getText());
+        for (int i = 0; i < countries.size(); i++) {
+            //System.out.println(countries.get(i).findElement(By.xpath(".//td[5]")).getText());
 
-            System.out.println(countries.get(i).findElement(By.xpath("//td[5]")).getText());
+            unsortedCountriesList.add(countries.get(i).findElement(By.xpath(".//td[5]")).getText());
+            sortedCountriesList.add(countries.get(i).findElement(By.xpath(".//td[5]")).getText());
 
             try {
-                String s = countries.get(i).findElement(By.xpath("//td[7]")).getText();
-                if (countries.get(i).findElement(By.xpath("//td[7]")).getText().length() != 0) {
-                    if (Integer.parseInt(countries.get(i).findElement(By.xpath("//td[7]")).getText()) > 0) {
-                        multiZoneCountry.add(countries.get(i).findElement(By.xpath("a")).getAttribute("href"));
-                    }
+                //System.out.println(countries.get(i).findElement(By.xpath(".//td[6]")).getText());
+
+                if (Integer.parseInt(countries.get(i).findElement(By.xpath(".//td[6]")).getText()) > 0) {
+                    System.out.println(countries.get(i).findElement(By.xpath(".//a")).getAttribute("href"));
+
+                    multiZoneCountry.add(countries.get(i).findElement(By.xpath(".//a")).getAttribute("href"));
                 }
             } catch (ClassCastException ex) {
                 System.out.println("Неверные данные в поле - error");
@@ -45,20 +46,23 @@ public class CheckSort extends BaseTests {
         Collections.sort(sortedCountriesList);
         ComparisonStringLists(unsortedCountriesList, sortedCountriesList);
 
-        for (int i = 1; i < multiZoneCountry.size(); i++) {
+        for (int i = 0; i < multiZoneCountry.size(); i++) {
             wd.get(multiZoneCountry.get(i));
 
             ArrayList<String> unsortedZonesList = new ArrayList<>();
             ArrayList<String> sortedZonesList = new ArrayList<>();
 
-            List<WebElement> zones = wd.findElements(By.xpath("//table[@id='table-zones']/tr"));
+            List<WebElement> zones = wd.findElements(By.xpath("//table[@id='table-zones']//tr"));
 
-            for (WebElement zone : zones) {
-                unsortedZonesList.add(zone.findElement(By.xpath("//td[3]")).getText());
-                sortedZonesList.add(zone.findElement(By.xpath("//td[3]")).getText());
+            System.out.println("Найдено " + zones.size() + " зон");
+
+            for (int ii = 1; ii < zones.size() - 1; ii++) {
+                //System.out.println(zones.get(ii).findElement(By.xpath(".//td[3]")).getText());
+
+                unsortedZonesList.add(zones.get(ii).findElement(By.xpath(".//td[3]")).getText());
+                sortedZonesList.add(zones.get(ii).findElement(By.xpath(".//td[3]")).getText());
             }
 
-            Collections.sort(sortedZonesList);
             ComparisonStringLists(unsortedZonesList, sortedZonesList);
 
         }
@@ -78,20 +82,45 @@ public class CheckSort extends BaseTests {
 
         System.out.println(countries.size());
 
-        for (int i = 1; i < countries.size(); i++) {
-            unsortedCountriesList.add(countries.get(i).findElement(By.xpath("//td[5]")).getText());
-            sortedCountriesList.add(countries.get(i).findElement(By.xpath("//td[5]")).getText());
+        for (int i = 0; i < countries.size(); i++) {
+            System.out.println(countries.get(i).findElement(By.xpath(".//td[3]")).getText());
 
-            System.out.println(countries.get(i).findElement(By.xpath("//td[5]")).getText());
+            unsortedCountriesList.add(countries.get(i).findElement(By.xpath(".//td[3]")).getText());
+            sortedCountriesList.add(countries.get(i).findElement(By.xpath(".//td[3]")).getText());
 
             try {
-                if (Integer.parseInt(countries.get(i).findElement(By.xpath("//td[7]")).getText()) > 0) {
-                    multiZoneCountry.add(countries.get(i).findElement(By.xpath("a")).getAttribute("href"));
+                if (Integer.parseInt(countries.get(i).findElement(By.xpath(".//td[4]")).getText()) > 0) {
+                    System.out.println(countries.get(i).findElement(By.xpath(".//a")).getAttribute("href"));
+
+                    multiZoneCountry.add(countries.get(i).findElement(By.xpath(".//a")).getAttribute("href"));
                 }
             } catch (ClassCastException ex) {
                 System.out.println("Неверные данные в поле - error");
             }
         }
+
+        Collections.sort(sortedCountriesList);
+        ComparisonStringLists(unsortedCountriesList, sortedCountriesList);
+
+//        for (int i = 0; i < multiZoneCountry.size(); i++) {
+//            wd.get(multiZoneCountry.get(i));
+//
+//            ArrayList<String> unsortedZonesList = new ArrayList<>();
+//            ArrayList<String> sortedZonesList = new ArrayList<>();
+//
+//            List<WebElement> zones = wd.findElements(By.xpath("//table[@id='table-zones']//tr"));
+//
+//            for (WebElement zone : zones) {
+//                System.out.println(zone.findElement(By.xpath(".//td[3]")).getText());
+//
+//                unsortedZonesList.add(zone.findElement(By.xpath(".//td[3]")).getText());
+//                sortedZonesList.add(zone.findElement(By.xpath(".//td[3]")).getText());
+//            }
+//
+//            Collections.sort(sortedZonesList);
+//            ComparisonStringLists(unsortedZonesList, sortedZonesList);
+//
+//        }
     }
 
     void ComparisonStringLists(ArrayList<String> unsortedList, ArrayList<String> sortedList) {
