882d80130aa9aecb235dd54f9da5f7249105cf34
Habsucht <aleksey.aka.gremlin@gmail.com>
2017-08-06 17:33:31 +0300

    Finalization class CheckActionsWithBasketGoods

--- java-example/src/test/java/ru/stqa/selenium_training/tests/ui/CheckActionsWithBasketGoods.java
+++ java-example/src/test/java/ru/stqa/selenium_training/tests/ui/CheckActionsWithBasketGoods.java
@@ -1,14 +1,21 @@
 package ru.stqa.selenium_training.tests.ui;
 
 import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
 
 public class CheckActionsWithBasketGoods extends BaseTests {
 
     @Test
     public void CheckActionsWithBasketGoods() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(wd, 10/*seconds*/);
 
         for (int i = 0 ; i < 3 ; i++) {
             wd.get("http://localhost/litecart/en/");
@@ -30,10 +37,10 @@ public class CheckActionsWithBasketGoods extends BaseTests {
             wd.findElement(By.name("add_cart_product")).click();
 
             for (; ; ) {
-                int checkProductInCartEnd2 = Integer.parseInt(wd.findElement(By.xpath("//div[@id='cart']//span[@class='quantity']")).getText());
+                int checkProductInCartEnd1 = Integer.parseInt(wd.findElement(By.xpath("//div[@id='cart']//span[@class='quantity']")).getText());
 
-                if (checkProductInCartEnd2 > checkProductInCartStart1) {
-                    System.out.println("Число товаров в корзине увеличилось на " + (checkProductInCartEnd2 - checkProductInCartStart1));
+                if (checkProductInCartEnd1 > checkProductInCartStart1) {
+                    System.out.println("Число товаров в корзине увеличилось на " + (checkProductInCartEnd1 - checkProductInCartStart1));
                     break;
                 } else {
                     System.out.println("WAIT...");
@@ -50,17 +57,21 @@ public class CheckActionsWithBasketGoods extends BaseTests {
         int checkProductInTableBeforeRemoval;
 
         for (int i = 0 ; i < checkProductInCartStart2 ; i++) {
-            checkProductInTableBeforeRemoval = wd.findElements(By.xpath("//table//td[@class='item']")).size();
+            wd.navigate().refresh();
+            ArrayList<WebElement> table = (ArrayList<WebElement>) wd.findElements(By.xpath("//table//td[@class='item']"));
+
+            checkProductInTableBeforeRemoval = table.size();
             wd.findElement(By.xpath("//button[@value='Remove']")).click();
 
             for (; ; ) {
                 int checkProductInTableAfterRemoval = wd.findElements(By.xpath("//table//td[@class='item']")).size();
+
                 if (checkProductInTableBeforeRemoval > checkProductInTableAfterRemoval) {
                     System.out.println("Число товаров в таблице уменьшилось до " + checkProductInTableAfterRemoval);
                     break;
                 } else {
                     System.out.println("WAIT...");
-                    Thread.sleep(1000);
+                    wait.until(stalenessOf(table.get(table.size() - 1)));
                 }
             }
         }
